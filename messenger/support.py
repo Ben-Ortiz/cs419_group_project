d5e29de485270268664df565de19c8bc164e78a9
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2021-10-21 09:37:18 -0400

    Update support.py

--- messenger/support.py
+++ messenger/support.py
@@ -1,4 +1,10 @@
-import os
+from os import system, name
 
+"""
+Function that clears the terminal of all text rendered
+"""
 def clear():
-    pass
+    if name == 'nt':
+        system('cls') # Windows Command
+    else:
+        system('clear') # Linux/Mac OS Command
