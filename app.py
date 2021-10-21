e2028145e0945397964d11336e58514a36ab15e5
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2021-10-21 10:05:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from tkinter import *
 
 root = Tk()
 root.title("Messenger App")
+root.geometry("400x400")
 
 vertical = Scale(root, from_=0, to=200)
 vertical.pack(side=RIGHT)
 
-
+vertical.get()
 
 root.mainloop()
