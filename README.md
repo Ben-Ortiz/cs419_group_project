41672bd50ba1294588bc2799fca7f04205c430ad
jayko8899 <jjarleth99@gmail.com>
2021-10-25 23:36:08 -0400

    added keys as letters and updated read me

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ encryption
 Basic Functionality:
 
 Baseline algorithm for encryption is the vigener cipher. Instead of using a key of letters that repeats, the algorithm uses a pseudorandom sequence equal to the length of the maximum message size that the messenger will allow. This will eliminate the kasisky test as a possible attack as sequence will not repeat. To add further randomness to the ciphertext, the algorithm will also insert a random character at every odd index of the ciphertext. This will make it that the same message encrypted with the same key will not be encrypted the same way twice thus making the algorithm non-deterministic.
+
+Future Plans:
+
+Instead of inserting numbers completely randomly, create a probability distribution that makes some letters much more likely to be inserted than others. This will create the illusion of patterns being present in the encrypted text. If an attacker were to try to run a frequency analysis on the ciphertext the frequencies of the letters will all be thrown off by the inserted letters. Note that for a frequency analysis to even be somewhat effective on this encryption scheme an extremly large amount of ciphertext woul need to be captured.
+
+There will also be multiple keys asscoiated with each user in the chatroom. How these keys will rotate I'm not sure of yet. Possible ideas are rotating the keys by message such that each message is encrypted by a different key until the keys repeat. Another possibility is to keep the server and client in sync timewise and rotate the keys every x amount of time. 
--- vigenere.py
+++ vigenere.py
@@ -3,15 +3,39 @@ import random
 import string
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#A-0, B-1, C-2, D-3, E-4, F-5, G-6, H-7, I-8, J-9, K-10, L-11, M-12, 
+#N-13, O-14, P-15, Q-16, R-17, S-18, T-19, U-20, V-21, W-22, X-23, Y-24, Z-25
 
 #debug variables to test various components
 DEBUG_VIG = 1
 DEBUG_RANDOM_KEY = 0
 DEBUG_INSERT_RAND_LETTERS = 0
+DEBUG_TEST_LETTER = 0
+
+#takes 2 letters and returns what lette they encrypt into
+def test_letter(a, b):
+
+    if a.upper() in LETTERS and b.upper() in LETTERS:
+
+        a_idx = LETTERS.find(a.upper())
+        b_idx = LETTERS.find(b.upper())
+
+        new_c_val = (a_idx + b_idx) % 26
+
+        return LETTERS[new_c_val].upper()
 
 
-#vigenere encryption algorithm with a non-random key
-def encrypt(m, k, is_rand_key):
+    else:
+        print("Invalid values passed to test_letter!\n")
+        exit
+
+
+#vigenere encryption algorithm
+#m - the message you want to encrypt
+#k - the key you want to use to encrypt m
+#key_type - the type of key you;re using: 1 for letters, 2 for numbers
+#insert_chars - whether or not you want to insert random letters every other character
+def encrypt(m, k, key_type, insert_chars):
     
     ct = ''                 #empty string to store ciphertext
     k_idx = 0               #index of the key letter in LETTERS
@@ -24,12 +48,15 @@ def encrypt(m, k, is_rand_key):
 
         if c.upper() in LETTERS:                            #ignore non-letter characters
 
-            c_idx = LETTERS.find(c.upper())                 #get the index of the letter to encrypt
+            c_idx = LETTERS.find(c.upper())                
 
-            if is_rand_key:                                 #check what type of key is being used
-                k_idx = int(k[k_loc])                       #key is random sequence of numbers
+            if key_type == 1:            
+                k_idx = LETTERS.find(k[k_loc].upper())                                 
+            elif key_type == 2:
+                k_idx = int(k[k_loc])
             else:
-                k_idx = LETTERS.find(k[k_loc].upper())      #key is letters
+                print("Invalid key type passed to encrypt!\n")
+                exit                                        
 
             new_c_val = (c_idx + k_idx) % 26                
 
@@ -44,13 +71,14 @@ def encrypt(m, k, is_rand_key):
         else:
             ct += c
 
-    ct = insert_rand_chars(ct)                              #make every other character a random letter
+    if insert_chars:
+        ct = insert_rand_chars(ct)                              #make every other character a random letter
 
     return ct
 
 #vigenere decryption algorithm
 #everything here is the same as encryption except we subtract the indexes, add 26, and take mod 26 to decrypt
-def decrypt(m, k, is_rand_key):
+def decrypt(m, k, key_type, insert_chars):
     
     pt = ''
     k_idx = 0
@@ -58,18 +86,25 @@ def decrypt(m, k, is_rand_key):
     k_loc = 0
     new_c_val = 0
     k_len = len(k)
+    msg = m
 
-    stripped_msg = strip_rand_chars(m)      #remove every other character as we know they are garbage
+    if insert_chars:
+        msg = strip_rand_chars(m)      #remove every other character as we know they are garbage
 
 
-    for c in stripped_msg:
+    for c in msg:
         if c.upper() in LETTERS:
 
             c_idx = LETTERS.find(c.upper())
-            if is_rand_key:                                 
-                k_idx = int(k[k_loc])                       
+
+            if key_type == 1:            
+                k_idx = LETTERS.find(k[k_loc].upper())                                 
+            elif key_type == 2:
+                k_idx = int(k[k_loc])
             else:
-                k_idx = LETTERS.find(k[k_loc].upper())
+                print("Invalid key type passed to encrypt!\n")
+                exit
+
             new_c_val = ((c_idx - k_idx) + 26) % 26         #do the opposite of the encryption algo
 
             if c.isupper():
@@ -85,15 +120,32 @@ def decrypt(m, k, is_rand_key):
 
     return pt
 
-#generate a random sequence of n numbers for use as a key
-def get_rand_key(n):
+#generate a random key
+#pass 1 to get a key of letters, pass 2 to get a key of numbers
+#n - the length of the key you want to generate
+def get_rand_key(n, type):
 
     k = ''
 
-    for __ in range(0, n):
-        k += str(random.randint(1, 9))
+    if type == 1:
 
-    return k
+        for __ in range(0, n):
+            k += random.choice(string.ascii_uppercase)
+        
+        return k
+    
+    elif type == 2:
+
+        for __ in range(0, n):
+            k += str(random.randint(1, 9))
+
+        return k
+
+    else:
+        
+        print("Invalid key type passed to get_rand_key!\n")
+        exit
+        
 
 #make every other character in a string a random character
 def insert_rand_chars(m):
@@ -125,29 +177,33 @@ def main():
     #this will test the encryption / decryption 
     if DEBUG_VIG == 1:
         n = 120
-        k_letters = 'LUCKY'
-        k_rand = get_rand_key(n)
+        key = get_rand_key(n, 1)
 
-        m = 'hello, my name is.'
-        ct = encrypt(m, k_rand, True)
-        pt = decrypt(ct, k_rand, True)
+        m = 'hello my name is josh'
+        ct = encrypt(m, key, 1, True)
+        pt = decrypt(ct, key, 1, True)
 
         print(f'| Message: {m} |\n| Ciphertext: {ct} |\n| Plaintext: {pt} |')
 
-        print(f'| Key: {k_rand} |')
+        print(f'| Key: {key} |')
 
     #this will test random key generator
     if DEBUG_RANDOM_KEY == 1:
         n = 120
-        key = get_rand_key(n)
+        key = get_rand_key(n, 1)
         print(key)
 
     #this will test the random letter inserter
     if DEBUG_INSERT_RAND_LETTERS == 1:
-        m = "hello my name is"
+        m = "suck it"
         s = insert_rand_chars(m, 1)
         print(s)
         print(strip_rand_chars(s))
+
+    #this will test the test_letter function
+    if DEBUG_TEST_LETTER:
+        print(test_letter('s', 'u'))
+        print(test_letter('u', 's'))
 
 
 if __name__ == "__main__":
