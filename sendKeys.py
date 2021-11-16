a2d05417d10edc6a2c8033cdc91f717d2858bb28
Benedict Ortiz <benortiz1391@gmail.com>
2021-11-15 22:59:04 -0500

    testing

--- sendKeys.py
+++ sendKeys.py
@@ -1,6 +1,13 @@
+#Diffie-Hellman Key Exchange inspired key exchange from client to server
+
 def main():
-    #TODO ask messenger team about how I would send keys from client to server
+    #TODO ask messenger team about how I would send keys from client to server if I have one implemented
     print("test")
+
+    key = np.random.randint(0, high = (2**64) - 1, dtype='uint64')
+    key = key.item()
+
+    print(key)
 
 if __name__ == "__main__":
     main()
